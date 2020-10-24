f7e693dd18d9955466701ae94f434e6c97ef1fae
github 0322keisuke <isikeidesk@gmail.com>
2020-10-25 06:55:45 +0900

    2-1 Topページ作成

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,1 +1,17 @@
-<template></template>
+<template>
+  <div>
+    <header>
+      My Trello
+    </header>
+    <main>
+      <p class="info-line">All:0 tasks</p>
+    </main>
+  </div>
+</template>
+
+
+<script>
+ export default {
+   
+ }
+</script>
