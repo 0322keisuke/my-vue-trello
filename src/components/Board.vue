7d2c09f71748f1e936170080a0b466c9cc61515c
github 0322keisuke <isikeidesk@gmail.com>
2020-10-25 20:22:28 +0900

    Cardのドラッグ＆ドロップ

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -11,6 +11,7 @@
               :title="item.title"
               :cards="item.cards"
               :listIndex="index"
+              @change="movingCard"
         /> 
        <list-add />
       </div>
@@ -29,7 +30,6 @@ import { mapState } from 'vuex'
      ListAdd,
      List,
    },
-
    computed:{
      ...mapState([
        'lists'
@@ -38,5 +38,10 @@ import { mapState } from 'vuex'
        return this.$store.getters.totalCardCount
      }
    },
+   methods:{
+     movingCard: function(){
+       this.$store.dispatch('updateList', {lists: this.lists})
+     },
+   }
  }
 </script>
